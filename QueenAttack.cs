fdd2f016f6ea57050cd992f0def1f9ea4434f096
Audrius Budrys <budrys@email.com>
2021-02-13 10:23:25 +0200

    Add project files.

---
+++ QueenAttack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        if (row >= 0 && row <= 7)
+        {
+            if (column >= 0 && column <= 7)
+            {
+               _= column;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _ = row;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        return new Queen(row, column);
+    }
+
+}
